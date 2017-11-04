0661b890ac072085b83be1781ceb1c82072452ac
imranabbas0007 <imranabbas0007@gmail.com>
2017-11-04 22:56:45 +0530

    Made changes to create database setup

--- interface/resources/scripts/main.js
+++ interface/resources/scripts/main.js
@@ -2,76 +2,76 @@ var app = angular.module('APPLICATION', []);
 app.config(['$qProvider', function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
 }]);
-app.controller('APPLICATIONCONTROLLER', function($scope, $http) {
-	$scope.password = '45678765';
-    $scope.email = 'hussain.hamdani@gmail.com';
-	$scope.loginAction = function() {
-		if($scope.email !== '' && $scope.password !== '') {
-			var data = {
-				email : $scope.email,
-				password : $scope.password
-			};
-			var config = {
-				url : 'login',
-				method: 'POST'
-			};
+app.controller('APPLICATIONCONTROLLER', function($scope, $http, $q) {
+    $scope.password = '';
+    $scope.email = '';
+    $scope.loginAction = function() {
+        if($scope.email !== '' && $scope.password !== '') {
+            var data = {
+                email : $scope.email,
+                password : $scope.password
+            };
+            var config = {
+                url : 'login',
+                method: 'POST'
+            };
+            serviceCall(config, data).then(function success(){
+				alert('Login Success');
+			},function error(){
+				alert('Login Error');
+			});
+        }
 
-			/*$http.get('http://localhost:8081/api/user', function(data) {
-				console.log(data);
-			});*/
-			serviceCall(config, data);
-		}
-		
-		function dataToString(data) {
-			var string = '';
-			for(var key in data) {
-				string = (string !== '') ? (string + '&' ): '';
-				string = string + key + '=' + data[key];
-			}
-			return string
-		}
+        function dataToString(data) {
+            var string = '';
+            for(var key in data) {
+                string = (string !== '') ? (string + '&' ): '';
+                string = string + key + '=' + data[key];
+            }
+            return string
+        }
 
-		function serviceCall(config, data) {
-			if(typeof config.url !== undefined && config.url !== null && config.url !== '') {
-				// Configuration object
-				var serviceConfiguration = {};
+        function serviceCall(config, data) {
+			var deferred = $q.defer();
+            if(typeof config.url !== undefined && config.url !== null && config.url !== '') {
+                // Configuration object
+                var serviceConfiguration = {};
 
-				// Generating url
-				serviceConfiguration.url = 'http://localhost:8081/api/' + config.url;
+                // Generating url
+                serviceConfiguration.url = 'http://localhost:8081/api/' + config.url;
 
-				// Appending Data To Service Request
-				if(typeof data !== 'undefined' && data !== null) {
-					serviceConfiguration.data = dataToString(data);
-				}
+                // Appending Data To Service Request
+                if(typeof data !== 'undefined' && data !== null) {
+                    serviceConfiguration.data = dataToString(data);
+                }
 
-				// Appending Method Type To Service Request
-				serviceConfiguration.method = (typeof config.method !== 'undefined' && config.method !== null) ? config.method : 'GET';
+                // Appending Method Type To Service Request
+                serviceConfiguration.method = (typeof config.method !== 'undefined' && config.method !== null) ? config.method : 'GET';
 
-				// Appending Header To Service Request
-				var headers = {
-				   //'Content-Type': 'application/x-www-form-urlencoded'
-				   'Content-Type': 'application/x-www-form-urlencoded'
-				};
-				serviceConfiguration.headers = (typeof config.headers !== 'undefined' && config.headers !== null) ? config.headers : headers;
+                // Appending Header To Service Request
+                var headers = {
+                   'Content-Type': 'application/x-www-form-urlencoded'
+                };
+                serviceConfiguration.headers = (typeof config.headers !== 'undefined' && config.headers !== null) ? config.headers : headers;
 
-				// Trigger api call
-				$http(serviceConfiguration).then(successCallback, errorCallback);
+                // Trigger api call
+                $http(serviceConfiguration).then(successCallback, errorCallback);
 
-				// Success call back
-				function successCallback(response) {
-					// this callback will be called asynchronously
-					// when the response is available
-					$http.get('http://localhost:8081/api/user', function(data) {
-						console.log(data);
-					});
-				}
+                // Success call back
+                function successCallback(response) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+					deferred.resolve(response);
+                }
 
-				// Error call back
-				function errorCallback(response) {
-					// called asynchronously if an error occurs
-					// or server returns response with an error status.
-				}
-			}
-		}
-	}
+                // Error call back
+                function errorCallback(response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+					deferred.reject(response);
+                }
+            }
+			return deferred.promise;
+        }
+    }
 });
--- services/resources/stub.js
+++ services/resources/stub.js
@@ -1,96 +1,71 @@
-var fs = require('fs');
+var mysql = require('mysql');
+var dbConnection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "gapp"
+});
 
-function rDatabase() {
-    var dbpath = 'resources/db/';
-    var folderSupprator = '/';
-    var relationalSupprator = '-';
-    var fileFormat = '.txt';
-
-    function createRecord(configuration) {
-        var tablePath = dbpath + configuration['DATABASE'];
-        if (!fs.existsSync(tablePath)) {
-            fs.mkdirSync(tablePath);
-        }
-
-        tablePath = tablePath + '/' + configuration['TABLE'];
-        if (!fs.existsSync(tablePath)) {
-            fs.mkdirSync(tablePath);
-        }
-
-        fs.readdir(tablePath, (err, files) => {
-            var lastInsertedId = 0;
-            if(err) {
-                throw err;
-            } else {
-                lastInsertedId = parseInt(files.length);
-             tablePath = tablePath + folderSupprator + configuration['TABLE'] + relationalSupprator;
-                insertRecprds(tablePath, configuration['RECORDS'], lastInsertedId);
-            }
-        });
+dbConnection.connect(function(err) {
+    if (err) {
+        return;
     }
+});
 
-    function insertRecprds(tablePath, records, lastInsertedId) {
-        if(records.length > 0) {
-            for(var key in  records) {
-                lastInsertedId++;
-                var record = records[key];
-                record['id'] = lastInsertedId;
-                var filePath = tablePath + lastInsertedId + fileFormat;
-                record = JSON.stringify(record);
-                fs.appendFile(filePath, record, function (err) {
-                  if (err) throw err;
-                });
-            }
-        }
-    }
+function runQuery(query, response) {
+	var result = {MESSAGE: 'Not Found'};
+	var resultStatus = 404;
+	dbConnection.query(query, function (error, result) {
+		if (!error){
+			result.DATA = result;
+			result.MESSAGE = 'SUCCESS';
+			response.status(200).json(result).end();
+		} else {
+			response.status(resultStatus).json(result).end();
+		}
+	});
+}
 
-    function getRecordById(configuration, res) {
-       var tablePath = dbpath + configuration['DATABASE'] + folderSupprator + configuration['TABLE'] + folderSupprator + configuration['TABLE'] + relationalSupprator + configuration['ID'] + fileFormat;
-       fs.readFile(tablePath, function(err, data) {
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          res.write(data);
-          res.end();
-       });
-    }
-
-    function deleteRecordById(configuration) {
-       var tablePath = dbpath + configuration['DATABASE'] + folderSupprator + configuration['TABLE'] + folderSupprator + configuration['TABLE'] + relationalSupprator + configuration['ID'] + fileFormat;
-       if(fileExists(tablePath)) {
-           fs.unlink(tablePath, function (err) {
-             if (err) throw err;
-           });
-       }
-       //console.log(tablePath);
-    }
-
-    function fileExists(filePath) {
-        try{
-            return fs.statSync(filePath).isFile();
-        } catch (err) {
-            return false;
-        }
-    }
-    
-    return {
-        deleteRecordById : deleteRecordById,
-        getRecordById: getRecordById,
-        createRecord: createRecord        
-    }
+function createSession(data, request, response, serverSession) {
+	var result = {MESSAGE: 'Service Error'};
+	var resultStatus = 404;
+	var query = "SELECT * FROM `users` WHERE `vEmail` = '"+data.email+"' AND `vPassword` = '"+data.password+"' AND `eStatus` = 'Active'";
+	dbConnection.query(query, function (error, result) {
+		if (!error){
+			if(typeof result !== 'undefined' && typeof result[0] !== 'undefined') {
+				serverSession.email = result[0].vEmail;
+				serverSession.userId = result[0].iUserId;
+				var json_response = {MESSAGE: 'SUCCESS'};
+				response.status(200).json(json_response).end();
+			}
+		} else {
+			response.status(resultStatus).json(result).end();
+		}
+	});
 }
 
 module.exports = function (app, serverSession, uniqid) {
-	
+
 	app.get('/api/user', function (request, response) {
 		var result = {MESSAGE: 'Not Found'};
 		var resultStatus = 404;
-		if(typeof serverSession !== 'undefined' && typeof serverSession.email !== 'undefined' && serverSession.email !== null) {
-			result.MESSAGE = 'SUCCESS';
-			result.DATA = {
-				email: serverSession.email
-			};
-			resultStatus = 200;
+		if(typeof serverSession !== 'undefined' && typeof serverSession.userId !== 'undefined' && serverSession.userId !== null) {
+			var sql = "SELECT * FROM `users` WHERE `iUserId` = '"+serverSession.userId+"' AND `eStatus` = 'Active'";
+			runQuery(sql, response);
+		} else {
+			response.status(resultStatus).json(result).end();
 		}
-		response.status(resultStatus).json(result).end();
+    });
+
+	app.get('/api/results', function (request, response) {
+		var result = {MESSAGE: 'Not Found'};
+		var resultStatus = 404;
+		if(typeof serverSession !== 'undefined' && typeof serverSession.userId !== 'undefined' && serverSession.userId !== null) {
+			var sql = "SELECT * FROM `genetic_results` WHERE `iUserId` = "+serverSession.userId;
+			runQuery(sql, response);
+		} else {
+			response.status(resultStatus).json(result).end();
+		}
     });
 
 	app.get('/api/logout',function(request,response){
@@ -98,71 +73,25 @@ module.exports = function (app, serverSession, uniqid) {
 		var resultStatus = 404;
 		request.session.destroy(function(err) {
 			if(!err) {
+				serverSession = {};
 				result.MESSAGE = 'SUCCESS';
 				resultStatus = 200;
+				response.status(resultStatus).json(result).end();
+			} else {
+				response.status(resultStatus).json(result).end();
 			}
 		});
-		response.status(resultStatus).json(result).end();
 	});
 
 	app.post('/api/login', function (request, response) {
-		console.log(request.session);
+		var result = {MESSAGE: 'Not Found'};
+		var resultStatus = 404;
 		if(typeof request.body.email !== 'undefined' && request.body.email !== '' && typeof request.body.password !== 'undefined' && request.body.password !== '') {
 			serverSession = request.session;
-			serverSession.email = request.body.email;
-			serverSession.password = request.body.password;
-			var json_response = {MESSAGE: 'SUCCESS'};
-			response.status(200).json(json_response).end();
+			createSession(request.body, request, response, serverSession);
 		} else {
 			var json_response = {MESSAGE: 'ERROR'};
 			response.status(404).json(json_response).end();
 		}
-    });
-	
-    // students
-    app.get('/api/users/:id', function (req, res) {
-        //req.body
-        // get record by id
-        var configuration = {
-            'DATABASE': 'rockon',
-            'TABLE': 'users',
-            'ID' : 2
-        };
-        getRecordById(configuration, res);
-    });
-
-    app.post('/api/users', function (req, res) {
-        // req.body
-      var configuration = {
-         'DATABASE': 'rockon',
-         'TABLE': 'users',
-         'RECORDS': [
-            {
-               name: 'Hussain',
-               email: 'hussain@gmail.com'
-            },
-            {
-               name: 'Abbas',
-               email: 'abbas@gmail.com'
-            },
-            {
-               name: 'Imran',
-               email: 'imran@gmail.com'
-            }
-         ]
-      };
-      createRecord(configuration);
-      res.json({'ststus' : 'CREATED'});
-    });
-
-    app.delete('/api/users/:id', function (req, res) {
-        // get record by id
-        configuration = {
-            'DATABASE': 'rockon',
-            'TABLE': 'users',
-            'ID' : req.params.id
-        };
-        deleteRecordById(configuration);
-        res.json({'ststus' : 'DELETED'});
     });
 };
